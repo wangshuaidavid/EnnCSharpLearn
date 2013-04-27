c4da72cc6b176b3bc7ad1ae1c80e5d56a3949658
wangshuai <wangshuai@yeah.net>
2013-04-27 18:07:32 +0800

    remove 多余代码

--- EnnCraftToolBox/NHibernateText/TestService.cs
+++ EnnCraftToolBox/NHibernateText/TestService.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using Servers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,23 +18,7 @@ namespace Domain
         [Test]
         public void InitTest()
         {
-            EnnEquipmentService equipService = new EnnEquipmentService();
-            int equipId1 = equipService.CreateEquipment("新加入的设备3", "description1");
-            int equipId2 = equipService.CreateEquipment("新加入的设备4", "description2");
-
-            GeneralDataAccess gda = new GeneralDataAccess();
-            EnnEquipment e1 = gda.GetById<EnnEquipment>(equipId1);
-            EnnEquipment e2 = gda.GetById<EnnEquipment>(equipId2);
-
-            //Assert.AreEqual("de", e1.EquipName);
-
-            equipService.AddConnectionToEquipments(e2.OutputPort1.Id, e1.InputPort2.Id);
-
-            //Assert.AreSame(e2.OutputPort1.RelevantIOConnection, e1.InputPort2.RelevantIOConnection);
-            //Assert.IsNull(e2.OutputPort2.RelevantIOConnection);
-
-            //equipService.removeConnectionFromEquipments(e2.OutputPort1.Id);
-            equipService.RemoveEquipment(equipId1);
+           
         }
         
 
