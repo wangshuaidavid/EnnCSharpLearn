4f18aabaef7dbd7384faea469e7433a591570eda
wangshuai <wangshuai@yeah.net>
2013-04-27 16:55:39 +0800

    fix equipment

--- EnnCraftToolBox/Domain/Repository/HeatPumpRepository.cs
+++ EnnCraftToolBox/Domain/Repository/HeatPumpRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Entities;
+using NHibernate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Domain.Repository
 {
@@ -14,24 +16,86 @@ namespace Domain.Repository
         {
             HeatPump returnHeatPump = null;
 
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                HeatPump equip = new HeatPump { EquipName = "HeatPump", EquipDescription = "description xxx" };
 
+                IOPort EleQIn = new IOPort();
+                EleQIn.PortName = "EleQIn";
+                EleQIn.Type = IOPortType.InputTypePort;
+                EleQIn.BelongsToEquipment = equip;
+
+                IOPort CwQIn = new IOPort();
+                CwQIn.PortName = "CwQIn";
+                CwQIn.Type = IOPortType.InputTypePort;
+                CwQIn.BelongsToEquipment = equip;
+
+
+                IOPort CoolingQOut = new IOPort();
+                CoolingQOut.PortName = "CoolingQOut";
+                CoolingQOut.Type = IOPortType.OutputTypePort;
+                CoolingQOut.BelongsToEquipment = equip;
+
+                IOPort LostQOut = new IOPort();
+                LostQOut.PortName = "LostQOut";
+                LostQOut.Type = IOPortType.OutputTypePort;
+                LostQOut.BelongsToEquipment = equip;
+
+                equip.EleQIn = EleQIn;
+                equip.CwQIn = CwQIn;
+
+                equip.CoolingQOut = CoolingQOut;
+                equip.LostQOut = LostQOut;
+
+                session.Save(equip);
+                transaction.Commit();
+                returnHeatPump = equip;
+            }
 
             return returnHeatPump;
         }
 
         public void Update(Equipment equip)
         {
-            throw new NotImplementedException();
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Update(equip);
+                transaction.Commit();
+            }
+        }
+
+        private void removePortRelevantConnectWithGivenSession(IOPort thePort, ISession session)
+        {
+            IOConnection conn = thePort.RelevantIOConnection;
+            if (conn == null)
+            {
+                return;
+            }
+            conn.SourcePort.RelevantIOConnection = null;
+            conn.SinkPort.RelevantIOConnection = null;
+            session.Delete(conn);
         }
 
         public void Remove(int equipId)
         {
-            throw new NotImplementedException();
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                EnnEquipment equip = session.Get<EnnEquipment>(equipId);
+
+                this.removePortRelevantConnectWithGivenSession(equip.InputPort1, session);
+                this.removePortRelevantConnectWithGivenSession(equip.InputPort2, session);
+                this.removePortRelevantConnectWithGivenSession(equip.OutputPort1, session);
+                this.removePortRelevantConnectWithGivenSession(equip.OutputPort2, session);
+                session.Delete(equip);
+                transaction.Commit();
+            }
         }
 
         public Equipment GetById(int equipId)
         {
-            throw new NotImplementedException();
+            EnnEquipment equipFromDb = null;
+            equipFromDb = session.Get<EnnEquipment>(equipId);
+            return equipFromDb;
         }
     }
 }
